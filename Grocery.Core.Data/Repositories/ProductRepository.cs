2bd9b154fdc5f35f58532ae0ca00a244cafce0bd
velocity968 <velo45145@gmail.com>
2025-10-07 12:36:22 +0200

    Added product prices
    Added price property to product class and visibility in product view

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -9,10 +9,10 @@ namespace Grocery.Core.Data.Repositories
         public ProductRepository()
         {
             products = [
-                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25)),
-                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30)),
-                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12)),
-                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31))];
+                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25), 1.99),
+                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30), 3.99),
+                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12), 0.99),
+                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31), 2.49)];
         }
         public List<Product> GetAll()
         {
