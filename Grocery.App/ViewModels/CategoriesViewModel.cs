2726f6134f73c5ab651049edaa72de2a01d57cf2
velocity968 <velo45145@gmail.com>
2025-10-07 21:36:29 +0200

    Added part 2 Categories
    Added CategoriesView + ViewModel
    Registered and connected view to app

---
+++ Grocery.App/ViewModels/CategoriesViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.App.Views;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using Grocery.Core.Services;
+using System.Collections.ObjectModel;
+
+namespace Grocery.App.ViewModels
+{
+    public partial class CategoriesViewModel : BaseViewModel
+    {
+        public ObservableCollection<Category> Categories { get; set; }
+        private readonly ICategoryService _categoryService;
+        [ObservableProperty]
+        Client client;
+
+        public CategoriesViewModel(ICategoryService categoryService, GlobalViewModel global)
+        {
+            Title = "Categorieen";
+            _categoryService = categoryService;
+            Categories = new(_categoryService.GetAll());
+            Client = global.Client;
+        }
+
+        [RelayCommand]
+        public async Task SelectCategory(Category category)
+        {
+            Dictionary<string, object> paramater = new() { { nameof(Category), category } };
+            await Shell.Current.GoToAsync($"{nameof(Views.CategoriesView)}?Titel={category.Name}", true, paramater);
+        }
+
+        public override void OnAppearing()
+        {
+            base.OnAppearing();
+            Categories = new(_categoryService.GetAll());
+        }
+
+        public override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Categories.Clear();
+        }
+    }
+}
