6b1b8f6871b1ed1809f9775cbed4695fa5320a48
velocity968 <velo45145@gmail.com>
2025-10-07 15:53:05 +0200

    Added part 1 Catagories
    Added
    * class Category
    * class ProductCategory
    * CategoryRepository + Interface
    * ProductCategoryRepository + Interface

---
+++ Grocery.Core.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using Grocery.Core.Interfaces.Repositories;
+using Grocery.Core.Models;
+
+namespace Grocery.Core.Data.Repositories
+{
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private readonly List<ProductCategory> productCategories;
+        public ProductCategoryRepository()
+        {
+            productCategories = [
+                new ProductCategory(1, 1, 1), 
+                new ProductCategory(2, 2, 1),
+                new ProductCategory(3, 3, 2),
+                new ProductCategory(4, 4, 2)
+                ];
+        }
+        public List<ProductCategory> GetAll()
+        {
+            return productCategories;
+        }
+
+        public ProductCategory? Get(int id)
+        {
+            return productCategories.FirstOrDefault(p => p.Id == id);
+        }
+
+        public ProductCategory Add(ProductCategory item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory? Delete(ProductCategory item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory? Update(ProductCategory item)
+        {
+            ProductCategory? productCategory = productCategories.FirstOrDefault(p => p.Id == item.Id);
+            if (productCategory == null) return null;
+            productCategory.Id = item.Id;
+            return productCategory;
+        }
+    }
+}
---
+++ Grocery.Core/Models/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Models
+{
+    public partial class ProductCategory : Category
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public ProductCategory(int id, int productId, int categoryId) : this(id, default!, productId, categoryId) { }
+        public ProductCategory(int id, string name, int productId, int categoryId) : base(id,  name)
+        {
+            ProductId = productId;
+            CategoryId = categoryId;
+        }
+    }
+}
